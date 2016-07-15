bd48fa3be3bbf0c9a3a9dde53a7c59165b7b0341
K Mahendar Reddy <mrkodimala@gmail.com>
2016-07-15 19:03:53 +0530

    Delete TestingPage.java

--- src/TestingPage.java
+++
@@ -1,9 +0,0 @@
-
-public class TestingPage {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String s=System.getProperty("user.home");
-		System.out.println(s);
-	}
-}
